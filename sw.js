0f3594067172e11ad491739faf9737ef740a32be
a7mdsali7 <a7mdtayb@gmail.com>
2026-04-14 05:10:25 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyALManFtdVMSpQLy0s5W6TaTkSnf5NjkBA",
+  authDomain: "zajil-249.firebaseapp.com",
+  projectId: "zajil-249",
+  storageBucket: "zajil-249.firebasestorage.app",
+  messagingSenderId: "624850591323",
+  appId: "1:624850591323:web:92692220b6b755a353a97e",
+  measurementId: "G-ZDE7QB1E6Z"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const CACHE = 'zajil-v1';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
